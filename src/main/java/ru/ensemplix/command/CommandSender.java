f0d9329d16ea32c48a5176837357f2ee44ff6eae
Vladislav Golub <ensirius@gmail.com>
2016-09-26 23:56:42 +0300

    Добавлен вспомогательный метод для отправки сообщений

--- src/main/java/ru/ensemplix/command/CommandSender.java
+++ src/main/java/ru/ensemplix/command/CommandSender.java
@@ -13,6 +13,16 @@ public interface CommandSender {
     void sendMessage(String message);
 
     /**
+     * Отправить сообщение пользователю с форматированием.
+     *
+     * @param message Сообщение, которое отправляем пользователю.
+     * @param args Аргументы, которые будут подставлены в строку.
+     */
+    default void sendMessage(String message, Object... args) {
+        sendMessage(String.format(message, args));
+    }
+
+    /**
      * Может ли пользователь выполнять указанную команду.
      *
      * @param command Команду, которую пытается выполнить пользователь.
