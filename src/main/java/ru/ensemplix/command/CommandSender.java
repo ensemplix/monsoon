04297b1bcf528ece3e3381eeb8984f631e2b2675
Vladislav Golub <ensirius@gmail.com>
2016-09-27 17:49:28 +0300

    Небольшие улучшения

--- src/main/java/ru/ensemplix/command/CommandSender.java
+++ src/main/java/ru/ensemplix/command/CommandSender.java
@@ -9,18 +9,9 @@ public interface CommandSender {
      * Отправить сообщение пользователю.
      *
      * @param message Сообщение, которое отправляем пользователю.
-     */
-    void sendMessage(String message);
-
-    /**
-     * Отправить сообщение пользователю с форматированием.
-     *
-     * @param message Сообщение, которое отправляем пользователю.
      * @param args Аргументы, которые будут подставлены в строку.
      */
-    default void sendMessage(String message, Object... args) {
-        sendMessage(String.format(message, args));
-    }
+    void sendMessage(String message, Object... args);
 
     /**
      * Может ли пользователь выполнять указанную команду.
@@ -29,8 +20,6 @@ public interface CommandSender {
      * @param action Действие, которе пытается выполнить пользователь.
      * @return {@code true}, если может выполнять команду.
      */
-    default boolean canUseCommand(String command, String action) {
-        return true;
-    }
+    boolean canUseCommand(String command, String action);
 
 }
--- src/test/java/ru/ensemplix/command/simple/SimpleSender.java
+++ src/test/java/ru/ensemplix/command/simple/SimpleSender.java
@@ -5,8 +5,8 @@ import ru.ensemplix.command.CommandSender;
 public class SimpleSender implements CommandSender {
 
     @Override
-    public void sendMessage(String message) {
-        System.out.println(message);
+    public void sendMessage(String message, Object... args) {
+        System.out.printf(message, args);
     }
 
     @Override
