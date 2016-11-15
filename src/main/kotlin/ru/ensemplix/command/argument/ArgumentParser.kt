f8693e4e60866fe769675581840032e2698e66e7
Vladislav Golub <ensirius@gmail.com>
2016-11-15 20:38:33 +0200

    Исправлена документация

--- src/main/kotlin/ru/ensemplix/command/argument/ArgumentParser.kt
+++ src/main/kotlin/ru/ensemplix/command/argument/ArgumentParser.kt
@@ -11,9 +11,9 @@ interface ArgumentParser<out T> {
 
     /**
      * Конвертация строки в аргумент.
-
+     *
      * @param value Строка, которую конвертируем в аргумент.
-     * *
+     *
      * @return Аргумент, который получился после конвертации.
      */
     fun parseArgument(value: String?): Argument<T>
